312aef5b10b91f65f23d3aff75022be083483602
Sooraj Rajkumar <soorajrk007@gmail.com>
2023-07-01 11:08:26 +0530

    form validation

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -26,23 +26,28 @@ const Contact = () => {
             style={{ maxWidth: "500px" }}
           >
             <input
-              type="text"
-              name="name"
-              placeholder="Enter your name"
-              className="p-2 mb-4 bg-transparent border-2 rounded-md text-white focus:outline-none"
-            />
-            <input
-              type="text"
-              name="email"
-              placeholder="Enter your email"
-              className="p-2 mb-4 bg-transparent border-2 rounded-md text-white focus:outline-none"
-            />
-            <textarea
-              name="message"
-              placeholder="Enter your message"
-              rows="10"
-              className="p-2 mb-4 bg-transparent border-2 rounded-md text-white focus:outline-none"
-            ></textarea>
+  type="text"
+  name="name"
+  placeholder="Enter your name"
+  class="p-2 mb-4 bg-transparent border-2 rounded-md text-white focus:outline-none"
+  required
+/>
+
+<input
+  type="email"
+  name="email"
+  placeholder="Enter your email"
+  class="p-2 mb-4 bg-transparent border-2 rounded-md text-white focus:outline-none"
+  required
+/>
+
+<textarea
+  name="message"
+  placeholder="Enter your message"
+  rows="10"
+  class="p-2 mb-4 bg-transparent border-2 rounded-md text-white focus:outline-none"
+  required
+></textarea>
 
             <button className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
               Let's Talk
